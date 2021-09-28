074d82e8c2a5980f884b2dffecf7f7f2f1fc4b7a
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-09-28 21:12:21 +0300

    Add files via upload

---
+++ kacMileEder.py
@@ -0,0 +1,12 @@
+# donusumOrani = 0.621371192
+# km = int(input("Kaç km?"))
+
+# mil = km * donusumOrani
+
+# print(str(km) +" Km = "+str(mil)+" mil eder")
+
+donusumOranii = 1.609344
+mill = int(input("Kaç mil? "))
+
+kmm = mill * donusumOranii
+print(str(mill) +" Mil = "+str(kmm)+" km eder ")
